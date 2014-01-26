02826b7d8454867e7434ceb766bfbb0361ee72e6
eirikjak <eirikjak@gmail.com>
2014-01-26 23:08:39 +0100

    Fires projectiles at nearbyEnemies

---
+++ UnityPruject/Assets/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	public float speed;
+	public Vector2 direction;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPos = this.transform.position;
+		newPos.x += direction.x * speed * Time.deltaTime;
+		newPos.y += direction.y * speed * Time.deltaTime;
+		this.transform.position = newPos;
+	}
+}
--- UnityPruject/Assets/Tower.cs
+++ UnityPruject/Assets/Tower.cs
@@ -6,10 +6,13 @@ public class Tower : MonoBehaviour {
 	public TargetPolicy Policy;
 	public float range;
 	public float fireIntervall;
+	public GameObject Projectile;
+
 	private bool reloading;
 	private Hashtable enemiesInRangeTable;
 	private List<GameObject> enemiesInRangeList;
 	private GameObject target;
+	private Timer reloadTimer;
 	// Use this for initialization
 	void Start () {
 		enemiesInRangeTable = new Hashtable();
@@ -23,10 +26,34 @@ public class Tower : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if(target)
+
+		if (reloadTimer != null)
+			reloadTimer.Update (Time.deltaTime);
+
+		if (target) {
 			LookAtTarget (target);
+			if(!reloading)
+				Fire();
+
+		}
 	}
 
+	void Fire(){
+		var projectile = (GameObject) Instantiate (Projectile);
+		projectile.transform.position = this.transform.position;
+		var projectileScript = projectile.GetComponent<Projectile> ();
+		projectileScript.direction = transform.TransformDirection(Vector2.up);
+		reloadTimer = new Timer (1);
+		reloadTimer.OnTimerFire += OnReloadTimer;
+		reloading = true;
+		reloadTimer.Start();
+
+
+	}
+
+	void OnReloadTimer(object sender, TimerEventArgs args){
+		reloading = false;
+	}
 	void LookAtTarget(GameObject target){
 	
 		float dx = target.transform.position.x -transform.position.x;
